11b1a88f67f2020c71e8f2d72736bca1daeb2b7d
Jerry Peng <pr2jerry@gmail.com>
2012-08-20 23:57:19 +0800

    Add ring middlewares for TR-069 protocol

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-ring "0.7.1"]]
-  :ring {:handler clj.tr069.core/tr069-handler
+  :ring {:handler clj.tr069.core/handler
          :port 8080}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.ws.commons.axiom/axiom-api "1.2.13"]
--- src/clj/tr069/core.clj
+++ src/clj/tr069/core.clj
@@ -1,9 +1,58 @@
 (ns clj.tr069.core
-  (:use (clj.tr069 databinding schema)))
+  (:use (clj.tr069 databinding schema))
+  (:import (java.io PushbackInputStream)))
 
-(defn tr069-handler [request]
+(defn- wrap-tr069-method [handler]
+  (fn [{method :request-method :as request}]
+    (cond
+      (= method :post) (handler request)
+      (= method :get) {:status 200
+                       :headers {"Content-Type" "text/plain"}
+                       :body "ACS is running"}
+      :else {:status 405})))
+
+(defn- wrap-tr069-exception [handler]
+  (fn [request]
+    (try
+      (handler request)
+      (catch Exception e
+        {:status 400
+         :headers {"Content-Type" "text/plain"}
+         :body (or (.getMessage e) "Bad request")}))))
+
+(defn- parse-message-or-nil
+  "Parse TR-069 message, or return nil if the content
+  is empty"
+  [body]
+  (let [in (PushbackInputStream. body)
+        first-byte (.read in)]
+    (if (not= -1 first-byte)
+      (do (.unread in first-byte)
+          (parse-tr069-message in))
+      nil)))
+
+(defn- wrap-tr069-message [handler]
+  (fn [{body :body :as request}]
+    (let [req-tr-msg (parse-message-or-nil body)
+          response (handler (assoc request :tr-message req-tr-msg))
+          resp-tr-msg (:tr-message response)]
+      (if (nil? resp-tr-msg)
+        (assoc-in response [:headers "Content-Length"] "0")
+        (let [resp-body (serialize-tr069-message resp-tr-msg)]
+          (-> response
+            (assoc-in [:headers "Content-Type"] "text/xml;charset=UTF-8")
+            (assoc-in [:headers "Content-Length"] (str (.length resp-body)))
+            (assoc :body resp-body)))))))
+
+(defn- cwmp-handler
+  "Core handler of TR-069 ACS"
+  [{message :tr-message :as request}]
   {:status 200
-   :headers {"Content-Type" "text/xml; charset=UTF-8"}
-   :body (serialize-tr069-message (create-tr069-message (->InformResponse 1)))
-   })
+   :tr-message message})
+
+(def handler
+  (-> cwmp-handler
+    wrap-tr069-message
+    wrap-tr069-exception
+    wrap-tr069-method))
 
