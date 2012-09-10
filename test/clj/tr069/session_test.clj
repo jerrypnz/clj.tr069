f25c93e51703da93be78aec9996add46ea61c24c
Jerry Peng <pr2jerry@gmail.com>
2012-09-10 11:42:01 +0800

    Format code so that the line length is less than 80 chars

--- test/clj/tr069/session_test.clj
+++ test/clj/tr069/session_test.clj
@@ -8,24 +8,28 @@
                              ParameterValueStruct)))
 
 (deftest new-tr069-session-test-remote-addr
-  (testing "Test failure: should use :remote-addr in request map"
+  (testing
+      "Test failure: should use :remote-addr in request map"
     (is (=
          {:state :initializing
           :remote-addr "172.16.0.111"}
          (new-tr069-session {:remote-addr "172.16.0.111"})))))
 
 (deftest new-tr069-session-test-x-forward-for
-  (testing "Test failure: should use X-Forwarded-For header to get remote address"
+  (testing
+      "Test failure: should use X-Forwarded-For header to get remote address"
     (is (=
          {:state :initializing
           :remote-addr "202.101.101.101"}
          (new-tr069-session {:remote-addr "172.16.0.111"
-                             :headers {"X-Forwarded-For" "202.101.101.101"}})))))
+                             :headers {"X-Forwarded-For"
+                                       "202.101.101.101"}})))))
 
 (def test-device-object (inform->device test-inform-object))
 
 (deftest establish-session-success
-  (testing "Test failure: session could not be established corretly"
+  (testing
+      "Test failure: session could not be established corretly"
     (is (=
          {:state :established
           :remote-addr "202.101.101.101"
@@ -36,7 +40,8 @@
                             test-inform-object)))))
 
 (deftest establish-session-incorrect-state
-  (testing "Test failure: incorrect session state before establishing"
+  (testing
+      "Test failure: incorrect session state before establishing"
     (is (thrown? IllegalStateException
                  (establish-session {:state :established
                                      :remote-addr "202.101.101.101"}
