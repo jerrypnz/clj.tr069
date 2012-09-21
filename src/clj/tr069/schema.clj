62d8f4a2c406a192da65fccef3d833ce72ed555b
Jerry Peng <pr2jerry@gmail.com>
2012-09-21 18:05:15 +0800

    Add TR-069 types

--- src/clj/tr069/schema.clj
+++ src/clj/tr069/schema.clj
@@ -2,9 +2,14 @@
   (:use (clj.tr069 datatype databinding util))
   (:require [clojure.string :as string]))
 
-; Fault
+;; Fault struct
 (deftr069type
-  ^:top-level
+  FaultStruct
+  (fault-code   :int    :FaultCode) 
+  (fault-string :string :FaultString))
+
+;; Fault
+(deftr069type ^:top-level
   Fault
   (fault-code   :int    :FaultCode) 
   (fault-string :string :FaultString)
@@ -15,9 +20,8 @@
   (fault-code   :int    :FaultCode :int) 
   (fault-string :string :FaultString))
 
-; Inform
-(deftr069type
-  ^:top-level
+;; Inform
+(deftr069type ^:top-level
   Inform
   (device-id      :child       :DeviceId)
   (events         :child-array :Event         :EventStruct)
@@ -43,11 +47,168 @@
   (product-class :string :ProductClass)
   (serial-number :string :SerialNumber))
 
-; Inform Response
-(deftr069type
-  ^:top-level
+;; Inform Response
+(deftr069type ^:top-level
   InformResponse
   (max-envelopes :int :MaxEnvelopes))
+
+;; Transfer Complete
+(deftr069type ^:top-level
+  TransferComplete
+  (command-key :string :CommandKey)
+  (fault :child :FaultStruct)
+  (start-time    :dateTime :StartTime)
+  (complete-time :dateTime :CompleteTime))
+
+(deftr069type ^:top-level
+  TransferCompleteResponse)
+
+;; Get RPC Methods
+(deftr069type ^:top-level
+  GetRPCMethods)
+
+;; Get RPC Methods Response
+(deftr069type ^:top-level
+  GetRPCMethodsResponse
+  (method-list :string-array :MethodList))
+
+;; Get Parameter Values
+(deftr069type ^:top-level
+  GetParameterValues
+  (parameter-names :string-array :ParameterNames))
+
+;; Get Parameter Values Response
+(deftr069type ^:top-level
+  GetParameterValuesResponse
+  (parameter-list :child-array :ParameterList :ParameterValueStruct))
+
+;; Get Parameter Names
+(deftr069type ^:top-level
+  GetParameterNames
+  (parameter-path :string  :ParameterPath)
+  (next-level     :boolean :NextLevel))
+
+;; Get Parameter Names Response
+(deftr069type ^:top-level
+  GetParameterNamesResponse
+  (parameter-list :child-array :ParameterList :ParameterInfoStruct))
+
+(deftr069type
+  ParameterInfoStruct
+  (name     :string  :Name)
+  (writable :boolean :Writable))
+
+;; Get Parameter Attributes
+(deftr069type ^:top-level
+  GetParameterAttributes
+  (parameter-names :string-array :ParameterNames))
+
+;; Get Parameter Attributes Response
+(deftr069type ^:top-level
+  GetParameterAttributesResponse
+  (parameter-list :child-array :ParameterAttributeStruct))
+
+(deftr069type
+  ParameterAttributeStruct
+  (name :string :Name)
+  (notification :int :Notification)
+  (access-list :string-array :AccessList))
+
+;; Set Parameter Values
+(deftr069type ^:top-level
+  SetParameterValues
+  (parameter-list :child-array :ParameterValueStruct))
+
+;; Set Parameter Values Response
+(deftr069type ^:top-level
+  SetParameterValuesResponse
+  (status :int :Status))
+
+;; Set Parameter Attributes
+(deftr069type ^:top-level
+  SetParameterAttributes
+  (parameter-list :child-array :SetParameterAttributesStruct))
+
+(deftr069type
+  SetParameterAttributesStruct
+  (name :string :Name)
+  (notification-change :boolean :NotificationChange)
+  (notification :int :Notification)
+  (access-list-change :boolean :AccessListChange)
+  (access-list :string-array :AccessList))
+
+;; Set Parameter Attributes Response
+(deftr069type ^:top-level
+  SetParameterAttributesResponse)
+
+;; Add Object
+(deftr069type ^:top-level
+  AddObject
+  (object-name :string :ObjectName)
+  (parameter-key :string :ParameterKey))
+
+;; Add Object Response
+(deftr069type ^:top-level
+  AddObjectResponse
+  (instance-number :unsignedInt :InstanceNumber)
+  (status :int :Status))
+
+;; Delete Object
+(deftr069type ^:top-level
+  DeleteObject
+  (object-name :string :ObjectName)
+  (parameter-key :string :ParameterKey))
+
+;; Delete Object Response
+(deftr069type ^:top-level
+  DeleteObjectResponse
+  (status :int :Status))
+
+;; Download
+(deftr069type ^:top-level
+  Download
+  (command-key :string :CommandKey)
+  (file-type   :string :FileType)
+  (url         :string :URL)
+  (username    :string :Username)
+  (password    :string :Password)
+  (file-size   :unsignedInt :FileSize)
+  (target-file-name :string :TargetFileName)
+  (delay-seconds :unsignedInt :DelaySeconds)
+  (success-url :string :SuccessURL)
+  (failure-url :string :FailureURL))
+
+;; Download Response
+(deftr069type ^:top-level
+  DownloadResponse
+  (status :int :Status)
+  (start-time :dateTime :StartTime)
+  (complete-time :dateTime :CompleteTime))
+
+;; Upload
+(deftr069type ^:top-level
+  Upload
+  (command-key :string :CommandKey)
+  (file-type   :string :FileType)
+  (url         :string :URL)
+  (username    :string :Username)
+  (password    :string :Password)
+  (delay-seconds :unsignedInt :DelaySeconds))
+
+;; Upload Response
+(deftr069type ^:top-level
+  UploadResponse
+  (status :int :Status)
+  (start-time :dateTime :StartTime)
+  (complete-time :dateTime :CompleteTime))
+
+;; Factory Reset
+(deftr069type ^:top-level
+  FactoryReset)
+
+(deftr069type ^:top-level
+  FactoryResetResponse)
+
 
 (defrecord Device
     [identifier
