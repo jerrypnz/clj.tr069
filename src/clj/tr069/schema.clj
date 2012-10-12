5f3361490b3c69d43842e68f74cd92b6b90cd7cd
Jerry Peng <pr2jerry@gmail.com>
2012-10-12 10:59:34 +0800

    Format code
    * src/clj/tr069/schema.clj: Format the deftr069type definitions

--- src/clj/tr069/schema.clj
+++ src/clj/tr069/schema.clj
@@ -3,26 +3,22 @@
   (:require [clojure.string :as string]))
 
 ;; Fault struct
-(deftr069type
-  FaultStruct
+(deftr069type FaultStruct
   (fault-code   :int    :FaultCode) 
   (fault-string :string :FaultString))
 
 ;; Fault
-(deftr069type ^:top-level
-  Fault
+(deftr069type ^:top-level Fault
   (fault-code   :int    :FaultCode) 
   (fault-string :string :FaultString)
   (details      :inline-array :SetParameterValuesFault))
 
-(deftr069type
-  SetParameterValuesFault
+(deftr069type SetParameterValuesFault
   (fault-code   :int    :FaultCode :int) 
   (fault-string :string :FaultString))
 
 ;; Inform
-(deftr069type ^:top-level
-  Inform
+(deftr069type ^:top-level Inform
   (device-id      :child       :DeviceId)
   (events         :child-array :Event         :EventStruct)
   (parameter-list :child-array :ParameterList :ParameterValueStruct)
@@ -30,107 +26,87 @@
   (current-time   :dateTime    :CurrentTime)
   (max-envelopes  :int         :MaxEnvelopes))
 
-(deftr069type
-  EventStruct
+(deftr069type EventStruct
   (command-key :string :CommandKey :string)
   (event-code  :string :EventCode  :string))
 
-(deftr069type
-  ParameterValueStruct
+(deftr069type ParameterValueStruct
   (name  :string :Name :string)
   (value :any-simple-value :Value))
 
-(deftr069type
-  DeviceId
+(deftr069type DeviceId
   (manufacturer :string :Manufacturer)
   (oui          :string :OUI)
   (product-class :string :ProductClass)
   (serial-number :string :SerialNumber))
 
 ;; Inform Response
-(deftr069type ^:top-level
-  InformResponse
+(deftr069type ^:top-level InformResponse
   (max-envelopes :int :MaxEnvelopes))
 
 ;; Transfer Complete
-(deftr069type ^:top-level
-  TransferComplete
+(deftr069type ^:top-level TransferComplete
   (command-key :string :CommandKey)
   (fault :child :FaultStruct)
   (start-time    :dateTime :StartTime)
   (complete-time :dateTime :CompleteTime))
 
-(deftr069type ^:top-level
-  TransferCompleteResponse)
+(deftr069type ^:top-level TransferCompleteResponse)
 
 ;; Get RPC Methods
-(deftr069type ^:top-level
-  GetRPCMethods)
+(deftr069type ^:top-level GetRPCMethods)
 
 ;; Get RPC Methods Response
-(deftr069type ^:top-level
-  GetRPCMethodsResponse
+(deftr069type ^:top-level GetRPCMethodsResponse
   (method-list :string-array :MethodList))
 
 ;; Get Parameter Values
-(deftr069type ^:top-level
-  GetParameterValues
+(deftr069type ^:top-level GetParameterValues
   (parameter-names :string-array :ParameterNames))
 
 ;; Get Parameter Values Response
-(deftr069type ^:top-level
-  GetParameterValuesResponse
+(deftr069type ^:top-level GetParameterValuesResponse
   (parameter-list :child-array :ParameterList :ParameterValueStruct))
 
 ;; Get Parameter Names
-(deftr069type ^:top-level
-  GetParameterNames
+(deftr069type ^:top-level GetParameterNames
   (parameter-path :string  :ParameterPath)
   (next-level     :boolean :NextLevel))
 
 ;; Get Parameter Names Response
-(deftr069type ^:top-level
-  GetParameterNamesResponse
+(deftr069type ^:top-level GetParameterNamesResponse
   (parameter-list :child-array :ParameterList :ParameterInfoStruct))
 
-(deftr069type
-  ParameterInfoStruct
+(deftr069type ParameterInfoStruct
   (name     :string  :Name)
   (writable :boolean :Writable))
 
 ;; Get Parameter Attributes
-(deftr069type ^:top-level
-  GetParameterAttributes
+(deftr069type ^:top-level GetParameterAttributes
   (parameter-names :string-array :ParameterNames))
 
 ;; Get Parameter Attributes Response
-(deftr069type ^:top-level
-  GetParameterAttributesResponse
+(deftr069type ^:top-level GetParameterAttributesResponse
   (parameter-list :child-array :ParameterAttributeStruct))
 
-(deftr069type
-  ParameterAttributeStruct
+(deftr069type ParameterAttributeStruct
   (name :string :Name)
   (notification :int :Notification)
   (access-list :string-array :AccessList))
 
 ;; Set Parameter Values
-(deftr069type ^:top-level
-  SetParameterValues
+(deftr069type ^:top-level SetParameterValues
   (parameter-list :child-array :ParameterValueStruct))
 
 ;; Set Parameter Values Response
-(deftr069type ^:top-level
-  SetParameterValuesResponse
+(deftr069type ^:top-level SetParameterValuesResponse
   (status :int :Status))
 
 ;; Set Parameter Attributes
-(deftr069type ^:top-level
-  SetParameterAttributes
+(deftr069type ^:top-level SetParameterAttributes
   (parameter-list :child-array :SetParameterAttributesStruct))
 
-(deftr069type
-  SetParameterAttributesStruct
+(deftr069type SetParameterAttributesStruct
   (name :string :Name)
   (notification-change :boolean :NotificationChange)
   (notification :int :Notification)
@@ -138,35 +114,29 @@
   (access-list :string-array :AccessList))
 
 ;; Set Parameter Attributes Response
-(deftr069type ^:top-level
-  SetParameterAttributesResponse)
+(deftr069type ^:top-level SetParameterAttributesResponse)
 
 ;; Add Object
-(deftr069type ^:top-level
-  AddObject
+(deftr069type ^:top-level AddObject
   (object-name :string :ObjectName)
   (parameter-key :string :ParameterKey))
 
 ;; Add Object Response
-(deftr069type ^:top-level
-  AddObjectResponse
+(deftr069type ^:top-level AddObjectResponse
   (instance-number :unsignedInt :InstanceNumber)
   (status :int :Status))
 
 ;; Delete Object
-(deftr069type ^:top-level
-  DeleteObject
+(deftr069type ^:top-level DeleteObject
   (object-name :string :ObjectName)
   (parameter-key :string :ParameterKey))
 
 ;; Delete Object Response
-(deftr069type ^:top-level
-  DeleteObjectResponse
+(deftr069type ^:top-level DeleteObjectResponse
   (status :int :Status))
 
 ;; Download
-(deftr069type ^:top-level
-  Download
+(deftr069type ^:top-level Download
   (command-key :string :CommandKey)
   (file-type   :string :FileType)
   (url         :string :URL)
@@ -179,15 +149,13 @@
   (failure-url :string :FailureURL))
 
 ;; Download Response
-(deftr069type ^:top-level
-  DownloadResponse
+(deftr069type ^:top-level DownloadResponse
   (status :int :Status)
   (start-time :dateTime :StartTime)
   (complete-time :dateTime :CompleteTime))
 
 ;; Upload
-(deftr069type ^:top-level
-  Upload
+(deftr069type ^:top-level Upload
   (command-key :string :CommandKey)
   (file-type   :string :FileType)
   (url         :string :URL)
@@ -196,18 +164,15 @@
   (delay-seconds :unsignedInt :DelaySeconds))
 
 ;; Upload Response
-(deftr069type ^:top-level
-  UploadResponse
+(deftr069type ^:top-level UploadResponse
   (status :int :Status)
   (start-time :dateTime :StartTime)
   (complete-time :dateTime :CompleteTime))
 
 ;; Factory Reset
-(deftr069type ^:top-level
-  FactoryReset)
+(deftr069type ^:top-level FactoryReset)
 
-(deftr069type ^:top-level
-  FactoryResetResponse)
+(deftr069type ^:top-level FactoryResetResponse)
 
 
 (defrecord Device
